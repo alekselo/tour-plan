b19784eb2e60b0ca07404d4f57a51ac5d6c2070c
alekselo <a.a.loktev@yandex.ru>
2020-09-24 23:30:55 +0300

    create: added reviews block and smth fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
 
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
@@ -15,6 +15,24 @@ var mySwiper = new Swiper(".swiper-container", {
   autoplay: {
     delay: 3000,
   },
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  // autoplay: {
+  //   delay: 3000,
+  // },
 });
 
 ymaps.ready(init);
