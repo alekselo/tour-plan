4be3c0879ec39a3c52c472b3838e1c36b536cbc0
alekselo <a.a.loktev@yandex.ru>
2020-10-05 23:21:06 +0300

    create: modal block + responsive

--- js/main.js
+++ js/main.js
@@ -71,10 +71,28 @@ $(document).ready(function () {
   });
 
   var modalButton = $("[data-toggle=modal]");
-  modalButton.on("click"),
-    function () {
-      console.log("Вы кликнули по кнопке с атрибутом data-toggle=modal");
-    };
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
 
 $(".newsletter").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+
+// modalButton.on("click"),
+//   function () {
+//     console.log("Вы кликнули по кнопке с атрибутом data-toggle=modal");
+//   };
